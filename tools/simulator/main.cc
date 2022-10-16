458cf032d5f82313bcff15feed2569edcc91f01b
Tako-San <dxe13@mail.ru>
2022-10-16 22:48:45 +0300

    Added example of cli11 usage

--- tools/simulator/main.cc
+++ tools/simulator/main.cc
@@ -1,7 +1,23 @@
+#include <CLI/App.hpp>
+#include <CLI/Config.hpp>
+#include <CLI/Formatter.hpp>
+
 #include <spdlog/spdlog.h>
 
-int main() {
-  spdlog::set_level(spdlog::level::info);
+int main(int argc, char **argv) {
+  CLI::App app{"Simulator"};
+
+  bool isInfo;
+  app.add_flag("-i,--info", isInfo, "Turn on info logging level");
+
+  try {
+    app.parse(argc, argv);
+  } catch (const CLI::ParseError &e) {
+    return app.exit(e);
+  }
+
+  spdlog::set_level(isInfo ? spdlog::level::info : spdlog::level::err);
+
   spdlog::info("Hello with info level");
   spdlog::error("Hello with error level");
   return 0;

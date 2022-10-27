dbfd2a9cdf3bf89848af42a9a0f5fa70b6fee897
ZVasilii <zaitsev.vl@phystech.edu>
2022-10-27 18:40:08 +0300

    Rework unit-tests and pageFaultHandler

--- src/memory/memory.cc
+++ src/memory/memory.cc
@@ -2,7 +2,7 @@
 
 namespace sim {
 
-Memory::Memory() {}
+Memory::Memory() : mem{}, stats{} {}
 
 Word Memory::loadWord(AddrType addr) {
 
@@ -26,6 +26,7 @@ void Memory::storeWord(AddrType addr, Word word) {
 
 Word Memory::pageFaultHandle(AddrType addr) {
 
+  (void)addr;
   stats.numPageFaults++;
   std::cout << "Placeholder for page fault" << std::endl;
   return kDummyWord;
@@ -33,13 +34,13 @@ Word Memory::pageFaultHandle(AddrType addr) {
 
 std::size_t Memory::getCurrMemSize() const { return mem.size(); }
 
-void Memory::printMemStats() const {
+void Memory::printMemStats(std::ostream &ost) const {
 
-  std::cout << "Memory stats:" << std::endl;
-  std::cout << "Loads: " << stats.numLoads << std::endl;
-  std::cout << "Stores: " << stats.numStores << std::endl;
-  std::cout << "PageFaults: " << stats.numPageFaults << std::endl;
-  std::cout << "End of memory stats." << std::endl;
+  ost << "Memory stats:" << std::endl;
+  ost << "Loads: " << stats.numLoads << std::endl;
+  ost << "Stores: " << stats.numStores << std::endl;
+  ost << "PageFaults: " << stats.numPageFaults << std::endl;
+  ost << "End of memory stats." << std::endl;
 }
 
 const Memory::MemoryStats &Memory::getMemStats() const { return stats; }

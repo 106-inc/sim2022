d156c8713668c8d6241a364aab1252beb79ad855
Bryanskiy <ivakin.kir@gmail.com>
2022-11-03 12:54:52 +0300

    [exec engine] fix casts

--- src/executor/executor.cc
+++ src/executor/executor.cc
@@ -2,6 +2,11 @@
 
 namespace sim {
 
+static auto applyOffset(RegVal val, RegVal imm) {
+  int64_t res = static_cast<int64_t>(val) + signCast(imm);
+  return static_cast<RegVal>(res);
+}
+
 static void executeAdd(const Instruction &inst, State &state) {
   RegVal rs1 = state.regs.get(inst.rs1);
   RegVal rs2 = state.regs.get(inst.rs2);
@@ -28,20 +33,19 @@ static void executeDiv(const Instruction &inst, State &state) {
 
 static void executeLw(const Instruction &inst, State &state) {
   RegVal rs1 = state.regs.get(inst.rs1);
-  RegVal offset = signCast(inst.imm);
-  state.regs.set(inst.rd, state.mem.loadWord(rs1 + offset));
+  state.regs.set(inst.rd, state.mem.loadWord(applyOffset(rs1, inst.imm)));
 }
 
 static void executeSw(const Instruction &inst, State &state) {
-  RegVal address = state.regs.get(inst.rs1) + signCast(inst.imm);
+  RegVal rs1 = state.regs.get(inst.rs1);
   RegVal rs2 = state.regs.get(inst.rs1);
-  state.mem.storeWord(address, rs2);
+  state.mem.storeWord(applyOffset(rs1, inst.imm), rs2);
 }
 
 static void executeJAL(const Instruction &inst, State &state) {
   state.branchIsTaken = true;
   state.regs.set(inst.rd, state.pc + kXLENInBytes);
-  state.npc = state.pc + signCast(inst.imm);
+  state.npc = applyOffset(state.pc, inst.imm);
 }
 
 static void executeJALR(const Instruction &inst, State &state) {
@@ -49,8 +53,8 @@ static void executeJALR(const Instruction &inst, State &state) {
   state.regs.set(inst.rd, state.pc + kXLENInBytes);
   RegVal rs1 = state.regs.get(inst.rs1);
   state.npc =
-      setBit<0, 0>(rs1 + signCast(inst.imm)); // setting the least-significant
-                                              // bit of the result to zero.
+      setBit<0, 0>(applyOffset(rs1, inst.imm)); // setting the least-significant
+                                                // bit of the result to zero.
 }
 
 Executor::Executor()

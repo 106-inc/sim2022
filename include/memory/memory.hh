b02e3b1fda728c4f7091104a6758f4660af51de1
ZVasilii <zcvasilii@gmail.com>
2022-12-13 13:00:38 +0300

    Fix damn CI

--- include/memory/memory.hh
+++ include/memory/memory.hh
@@ -297,4 +297,5 @@ inline void TLB::tlbUpdate(Addr addr, listIt page) {
 inline const TLB::TLBStats &TLB::getTLBStats() { return stats; }
 } // namespace sim
 
+//dummy comment
 #endif // __INCLUDE_MEMORY_MEMORY_HH__

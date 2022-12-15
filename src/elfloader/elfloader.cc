e248b3bd881d50399fba6aeaa2aecb5f353ea15e
Tako-San <dxe13@mail.ru>
2022-12-15 23:14:51 +0300

    Added by-segment load

--- src/elfloader/elfloader.cc
+++ src/elfloader/elfloader.cc
@@ -1,5 +1,6 @@
 #include <stdexcept>
 
+#include "common/common.hh"
 #include "elfloader/elfloader.hh"
 
 namespace sim {
@@ -35,6 +36,41 @@ Addr ELFLoader::getSectionAddr(const std::string &name) const {
 
 bool ELFLoader::hasSection(const std::string &name) const {
   return elfFile_.sections[name] != nullptr;
+}
+
+std::vector<ELFLoader::IndexT> ELFLoader::getLoadableSegments() const {
+  std::vector<IndexT> res{};
+  for (auto *segment : elfFile_.segments)
+    if (ELFIO::PT_LOAD == segment->get_type())
+      res.push_back(segment->get_index());
+  return res;
+}
+
+std::span<const Word> ELFLoader::getSegment(IndexT index) const {
+  auto *segment = getSegmentPtr(index);
+
+  auto *data = reinterpret_cast<const Word *>(segment->get_data());
+  constexpr auto factor = sizeof(Word) / sizeof(char);
+
+  return std::span<const Word>{data, segment->get_memory_size() / factor};
+}
+
+Addr ELFLoader::getSegmentAddr(IndexT index) const {
+  auto *segment = getSegmentPtr(index);
+  return static_cast<Addr>(segment->get_virtual_address());
+}
+
+bool ELFLoader::hasSegment(IndexT index) const {
+  return elfFile_.segments[index] != nullptr;
+}
+
+const ELFIO::segment *ELFLoader::getSegmentPtr(IndexT index) const {
+  auto *segment = elfFile_.segments[index];
+
+  if (segment == nullptr)
+    throw std::runtime_error{"Unknown segment index: " + std::to_string(index)};
+
+  return segment;
 }
 
 const ELFIO::section *ELFLoader::getSectionPtr(const std::string &name) const {

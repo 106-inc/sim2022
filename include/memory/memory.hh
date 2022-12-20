56afeccd1d6dd1874acaddae53535bf77e4dbbd7
ZVasilii <zcvasilii@gmail.com>
2022-12-20 15:10:39 +0300

    REsolve conflicts_V4

--- include/memory/memory.hh
+++ include/memory/memory.hh
@@ -216,6 +216,9 @@ inline Word Memory::loadWord(Addr addr) {
 inline void Memory::storeWord(Addr addr, Word word) {
   stats.numStores++;
   *physMem.getEntity<Word, PhysMemory::MemoryOp::STORE>(addr) = word;
+  if (isProgramStored) {
+    cosimLog("M[0x{:08x}]=0x{:08x}", addr, word);
+  }
 }
 
 inline Half Memory::loadHalf(Addr addr) {

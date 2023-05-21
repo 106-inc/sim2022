732667914d75f67ae179751f8fb7d9f5139366df
derzhavin3016 <derzhavin.andrej@yandex.ru>
2023-05-21 23:30:07 +0300

    [dectool] Improve output

--- tools/dectool/main.cc
+++ tools/dectool/main.cc
@@ -1,5 +1,6 @@
 #include <exception>
 #include <filesystem>
+#include <iomanip>
 #include <iostream>
 
 #include <CLI/CLI.hpp>
@@ -7,6 +8,16 @@
 #include "common/common.hh"
 #include "decoder/decoder.hh"
 #include "elfloader/elfloader.hh"
+
+sim::Word toBE(sim::Word num) {
+  static_assert(sizeof(decltype(num)) == 4);
+  auto byte0 = (num & 0x000000ffu) << 24u;
+  auto byte1 = (num & 0x0000ff00u) << 8u;
+  auto byte2 = (num & 0x00ff0000u) >> 8u;
+  auto byte3 = (num & 0xff000000u) >> 24u;
+
+  return byte0 | byte1 | byte2 | byte3;
+}
 
 int main(int argc, char *argv[]) try {
   namespace fs = std::filesystem;
@@ -24,12 +35,12 @@ int main(int argc, char *argv[]) try {
   auto text_sec = elf_loader.getTextSection();
 
   auto *start = text_sec->get_data();
-  auto offset = text_sec->get_offset();
   for (std::size_t i = 0; i < text_sec->get_size(); i += sizeof(sim::Word)) {
     auto bin_word = *reinterpret_cast<const sim::Word *>(start + i);
 
     auto inst = sim::Decoder::decode(bin_word);
-    std::cout << "0x" << std::hex << offset + i << std::dec
+    std::cout << std::hex << std::setw(4) << std::setfill('0') << i << "  "
+              << std::setw(8) << toBE(bin_word) << std::dec
               << " :: " << inst.str() << std::endl;
   }
 
